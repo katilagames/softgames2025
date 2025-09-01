f25e0a7d155349ec19e1d5c610f6357049e91b83
Michal Cieslak <michal@dorian.live>
2025-09-01 20:04:00 +0200

    Update configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,49 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  entry: "./src/index.ts",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "build"),
-  },
-  mode: "development",
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: "ts-loader", exclude: /node_modules/ },
-      { test: /\.js$/, loader: "source-map-loader" },
+module.exports = (env) => {
+  return {
+    entry: "./src/index.ts",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "build"),
+      clean: true
+    },
+    mode: "development",
+    devtool: "source-map",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: "ts-loader", exclude: /node_modules/ },
+        { test: /\.js$/, loader: "source-map-loader" },
+        { test: /\.css$/i, use: ["style-loader", "css-loader"]}
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'My journey with Softgames starts here',
+        filename: 'index.html'
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: "src/img/", to: "img/" }
+        ]
+      }),
+      new webpack.DefinePlugin({
+        'process.env.MODE': `"${env.MODE}"`,
+      }),
+      new Dotenv(),
     ],
-  },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'My journey with Softgames starts here',
-    filename: 'index.html'
-  })],
-  target: 'web'
+    target: 'web',
+    devServer: {
+      devMiddleware: {
+        writeToDisk: true,
+      }
+    }
+  }
 };
